e54e1feaa73db4f67a5af7271f8f8a84a2952903
jackpots28 <xblackjack28x@gmail.com>
2024-04-25 08:41:03 -0500

    First commit

---
+++ test_file_input_python/python_script.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, redirect, url_for, send_from_directory,render_template
+from werkzeug.utils import secure_filename
+
+from docx import Document
+from docx.enum.text import WD_COLOR_INDEX
+
+import os
+
+UPLOAD_FOLDER = os.path.abspath("./uploads/")
+ALLOWED_EXTENSIONS = set(["txt", "text", "docx","pdf"])
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if not "file" in request.files:
+            return "No file part in the form."
+        f = request.files["file"]
+        if f.filename == "":
+            return "No file selected."
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return "file successfully upload"
+        return "File not allowed."
+    return "Upload file route"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
