557c1e76222fee18680b129220475ef528a2aef6
Jack Sims <xblackjack28x@gmail.com>
2024-05-02 23:04:32 -0500

    Started testing simularity searching between sentences in json payload/lists

---
+++ hugging_face_testing/hugging_face_scratch_1.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import json
+import requests
+import os
+
+path_to_json = Path("./json_payloads/test_1.json")
+input_payload = json.load(open(path_to_json))
+
+api_token = os.environ['HF_API_TOKEN']
+print(api_token[0:2])
+
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+headers = {"Authorization": f"Bearer {api_token}"}
+
+
+def query(payload: json) -> json:
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+data = query(input_payload)
+
+print(data)
