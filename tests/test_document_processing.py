3b99a5ba36c2f1e3476bcdf28f5b6ce6d632e10e
jackpots28 <xblackjack28x@gmail.com>
2024-05-06 10:49:48 -0500

    Fixed abs path issue on imports

--- tests/test_document_processing.py
+++ tests/test_document_processing.py
@@ -1,13 +1,16 @@
-from src.processing import document_processor
 from pathlib import Path
-from src.logger import logger
-
 from docx import Document
 from docx.enum.text import WD_COLOR_INDEX
 
 import unittest
 import docx
+import os, sys
 
+project_root = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(project_root))
+
+from src.logger import logger
+from src.processing import document_processor
 
 class TestDocumentProcessor(unittest.TestCase):
     def test_splitting_sentences_to_list(self):
