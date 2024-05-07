0e1c43543179ee016fdc8301d53053ecd7b74c2a
jackpots28 <xblackjack28x@gmail.com>
2024-05-07 13:38:31 -0500

    Started on file_handling seperation and updating path sanitization in web_app

--- src/file_transactions/file_handler.py
+++ src/file_transactions/file_handler.py
@@ -1,6 +1,27 @@
 from pathlib import Path
+from werkzeug.utils import secure_filename
+
+import os, sys
 
 '''
 TODO - need to start writing this file handler for sanitization 
 and verification of file paths / data ingestion typing
 '''
+
+ALLOWED_EXTENSIONS = {"doc", "docx"}
+
+project_root = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(project_root))
+
+from src.logger import logger
+from src.logger.logger import func_log
+
+log_path = Path(f"{project_root}/logs/file_handler.log")
+file_handler_logger = logger.setup_logger("file_handler_logger", log_path)
+
+@func_log
+def allowed_file(filename: str) -> bool:
+    allowed_file_output = "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    file_handler_logger.debug(f"Input allow_file filename var type: {type(filename)}")
+    file_handler_logger.debug(f"{allowed_file_output} - Type: {type(allowed_file_output)}")
+    return allowed_file_output
--- web_app.py
+++ web_app.py
@@ -18,7 +18,7 @@ log_path = Path(f"{project_root}/logs/web_app.log")
 web_app_logger = logger.setup_logger("web_app_logger", log_path)
 
 UPLOAD_FOLDER = Path(f"{project_root}/output_files")
-ALLOWED_EXTENSIONS = {"txt", "text", "docx", "pdf"}
+ALLOWED_EXTENSIONS = {"doc", "docx"}
 
 @func_log
 def allowed_file(filename: str) -> bool:
@@ -41,13 +41,17 @@ def index():
 @app.route("/upload", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
-        if not "file" in request.files:
-            return "No file part in the form."
+        web_app_logger.debug(f"Contents of request.files: {request.files}")
+        if "file" not in request.files:
+            return "No file in the form."
         f = request.files["file"]
         if f.filename == "":
             return "No file selected."
+        
+        web_app_logger.debug(f"Paths in request.file array: {f}")
         if f and allowed_file(f.filename):
             filename = secure_filename(str(f.filename))
+            web_app_logger.debug(f"secure_filename contents: {filename}")
             # TODO - Redo this to utilize an external class object to create documents, parse, and update/save them
             doc = docx.Document(filename)
             word = request.form['text']
